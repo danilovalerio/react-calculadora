c80a9a867731b67bcb0dcd1577f5dcd70e6a3771
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-03 16:12:53 -0300

    Componente Botão e CSS com variáveis

--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import './Calculadora.css'
 
+import Button from '../components/Button'
+
 export default class Calculadora extends Component {
     render(){
         return(
             <div className="calculadora">
+                <Button/>
+                <Button/>
+                <Button/>
+                <Button/>
+                <Button/>
+                <Button/>
+                <Button/>
+                <Button/>
+
             </div>
         )
     }
