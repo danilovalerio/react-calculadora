24d25ca1e7dd26d23886f6b43e40acb64d5bc77a
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-03 00:50:21 -0300

    Componente - criado o componente calulador

---
+++ src/main/Calculadora.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Calculadora.css'
+
+export default class Calculadora extends Component {
+    render(){
+        return(
+            <div className="calculadora">
+            </div>
+        )
+    }
+}
