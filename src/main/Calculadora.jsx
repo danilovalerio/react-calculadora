86bc361aec415fb4ba897338fbf9c15102c6bd5f
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-03 16:20:23 -0300

    Ajuste no grid dos button's

--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -7,14 +7,24 @@ export default class Calculadora extends Component {
     render(){
         return(
             <div className="calculadora">
-                <Button/>
-                <Button/>
-                <Button/>
-                <Button/>
-                <Button/>
-                <Button/>
-                <Button/>
-                <Button/>
+                <Button label="AC"/>
+                <Button label="/"/>
+                <Button label="7"/>
+                <Button label="8"/>
+                <Button label="9"/>
+                <Button label="*"/>
+                <Button label="4"/>
+                <Button label="53"/>
+                <Button label="6"/>
+                <Button label="-"/>
+                <Button label="1"/>
+                <Button label="2"/>
+                <Button label="3"/>
+                <Button label="+"/>
+                <Button label="0"/>
+                <Button label="."/>
+                <Button label="="/>
+              
 
             </div>
         )
