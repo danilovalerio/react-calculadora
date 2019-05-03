874de69024bb04c3a490959f81272e95a394fae6
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-03 19:04:54 -0300

    Estilizando o layout e deixando os componentes prontos para receberem a lógica

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,4 +2,15 @@ import React from 'react'
 import './Button.css'
 
 export default props =>
-    <button className='button'>{props.label}</button>
+    <button 
+        onClick={e => props.click && props.click(props.label)}
+        className={`
+            // aqui dentro podemos js ao invés de jsx para tratar exibição opcional de classes
+            button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+        
+        `}>
+        {props.label}
+    </button>
